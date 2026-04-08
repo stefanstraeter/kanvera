3a1b624927975a6b54e25d0ca26be1e62ce74a2d
Stefan Sträter <stefan.straeter@gmail.com>
2026-04-08 15:13:04 +0200

    refactor: rename script files to kebab-case for consistency

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // src/main.js
-import { Navbar } from './components/layout/Navbar.js';
-import { Header } from './components/layout/Header.js';
+import { Navbar } from './components/layout/navbar.js';
+import { Header } from './components/layout/header.js';
 
 async function initLayout() {
     const navbar = new Navbar();
