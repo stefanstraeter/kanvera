a407f614d2a24b6cfa258b1416288048c12c3a3c
Stefan Sträter <stefan.straeter@gmail.com>
2026-04-12 13:47:16 +0200

    style: add authentication error messages for improved user feedback

---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+/** 
+ * @description A collection of error messages related to authentication processes, such as login and signup.
+ * 
+*/
+export const AUTH_ERRORS = {
+    NAME: "Your name is required.",
+    EMAIL_LOGIN: "Please enter your email.",
+    EMAIL_SIGNUP: "Please enter a valid email.",
+    PASSWORD_LOGIN: "Please enter your password.",
+    PASSWORD_SIGNUP: "At least 8 characters.",
+    PASSWORD_CONFIRM: "Passwords do not match.",
+    INVALID_AUTH: "Invalid email or password.",
+    POLICY: "Please accept the privacy policy."
+};
+
