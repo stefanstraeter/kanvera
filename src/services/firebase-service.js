48e89635412ec1b677fab4a03e4bc6a6402138ff
Stefan Sträter <stefan.straeter@gmail.com>
2026-04-13 13:30:16 +0200

    style: add firebase-service.js for data fetching and posting to Firebase Realtime Database

---
+++ src/services/firebase-service.js
@@ -0,0 +1,43 @@
+// src/services/firebase-service.js
+
+const BASE_URL = "https://join-7c944-default-rtdb.europe-west1.firebasedatabase.app/";
+
+/**
+ * Fetches data from the Firebase Realtime Database.
+ * @param {string} [path=""] The path in the database to fetch data from.
+ * @return {Promise<null|any>} The fetched data as a JSON object, or null if an error occurred.
+ */
+export async function getData(path = "") {
+    try {
+        let response = await fetch(BASE_URL + path + ".json");
+        let responseToJson = await response.json();
+        return responseToJson;
+    } catch (error) {
+        console.error("Error loading data:", error);
+        return null;
+    }
+}
+
+/**
+ * Posts data to the Firebase Realtime Database.
+ * @param {string} [path="users"] The path in the database to post data to.
+ * @param {Object} [data={}] The data to be posted.
+ * @return {Promise<any>} The response from the server as a JSON object.    
+ */
+export async function postData(path, data) {
+    try {
+        let response = await fetch(BASE_URL + path + ".json", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        });
+        return await response.json();
+    } catch (error) {
+        console.error("Error loading data:", error);
+    }
+}
+
+
+
