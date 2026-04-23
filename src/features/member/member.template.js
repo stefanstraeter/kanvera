6d93d4526b3b99b2190c8bf2da34c59cba5aaa1f
Stefan Sträter <stefan.straeter@gmail.com>
2026-04-23 15:52:53 +0200

    feat(member-template): add member template module for card and modal HTML exports

---
+++ src/features/member/member.template.js
@@ -0,0 +1,4 @@
+// src/features/member/member.template.js
+
+export { createMemberCardHtml } from './member-card.template.js';
+export { createEditModalHtml, createConfirmDeleteHtml, createAddMemberModalHtml } from './member-modal.template.js';
