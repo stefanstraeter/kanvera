cb81593e503b7a9cb96a180fbd6e0b3fc0fdb44d
Stefan Sträter <stefan.straeter@gmail.com>
2026-04-23 15:55:49 +0200

    feat(task-template): update exports to include createTaskDetailCardHtml and createConfirmDeleteTaskHtml

--- src/features/task/task.template.js
+++ src/features/task/task.template.js
@@ -1,4 +1,4 @@
 // src/features/task/task.template.js
 
 export { createTaskCardHtml, createAvatarHtml } from './task-card.template.js';
-export { createTaskDetailHtml } from './task-modal.template.js';
+export { createTaskDetailCardHtml, createConfirmDeleteTaskHtml } from './task-modal.template.js';
