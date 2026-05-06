3811deeb8569f984cde7514aedad597fe7b714a3
Stefan Sträter <stefan.straeter@gmail.com>
2026-05-06 18:14:40 +0200

    feat(task.template): add createAddTaskModalHtml export to task modal templates

--- src/features/task/task.template.js
+++ src/features/task/task.template.js
@@ -1,4 +1,4 @@
 // src/features/task/task.template.js
 
 export { createTaskCardHtml, createAvatarHtml } from './task-card.template.js';
-export { createTaskDetailCardHtml, createConfirmDeleteTaskHtml } from './task-modal.template.js';
+export { createTaskDetailCardHtml, createConfirmDeleteTaskHtml, createAddTaskModalHtml } from './task-modal.template.js';
