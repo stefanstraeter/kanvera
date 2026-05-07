01aea71f8632832846cd27e7463904250c34541d
Stefan Sträter <stefan.straeter@gmail.com>
2026-05-07 13:07:04 +0200

    refactor(components): remove unused generateSubtasksHtml export from components.template.js

--- src/features/task/templates/components.template.js
+++ src/features/task/templates/components.template.js
@@ -1,3 +1,3 @@
 export { renderPrioritySelector } from './components/priority.template.js';
 export { renderAssigneeSelector, createDropdownHtml, renderItem } from './components/assignee.template.js';
-export { renderSubtasksList, generateSubtasksHtml } from './components/subtask.template.js';
+export { renderSubtasksList } from './components/subtask.template.js';
